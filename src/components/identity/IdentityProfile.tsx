01b96f38cc1026e256300642116a9bd997f04679
Liran Cohen <c.liran.c@gmail.com>
2024-10-08 11:20:12 -0400

    initial layout and profile page

---
+++ src/components/identity/IdentityProfile.tsx
@@ -0,0 +1,141 @@
+import React, { useMemo } from 'react';
+import { Identity } from '@/lib/types';
+import { DwnPermissionGrant, DwnProtocolDefinition } from '@web5/agent';
+
+export function generateGradient(byteString: string) {
+  if (byteString.length < 32) {
+    throw new Error("Input must be at least 32 bytes long.");
+  }
+
+  const colorNumber = 3;
+  const colorSeparation = 60;
+  const byteValues = Array.from(byteString, (char) => char.charCodeAt(0));
+  const saturation = 80 + (byteValues[0] % 15); // Saturation between 85-100%
+  const lightness = 45 + (byteValues[1] % 20); // Lightness between 50-70%
+  const baseHue = (byteValues[2] * 1.4) % 360;
+
+  // Generate three colors, skipping a colorSeparation distance on the color wheel
+  const bands = Array.from({ length: colorNumber }).map((_, i) => {
+    const hue = (baseHue + i * colorSeparation) % 360; // Adjust hue with offset
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+  }).join(', ');
+
+  let gradientAngle = byteValues[3] % 180; // Initial angle range between 0-179 degrees
+
+  // Shift the angle to fit within the left or right side constraints (45-135 and 225-315 degrees)
+  gradientAngle = gradientAngle < 45 ? gradientAngle += 315 : gradientAngle > 135 ? gradientAngle += 90 : gradientAngle;
+
+  return `linear-gradient(${gradientAngle}deg, ${bands})`;
+
+}
+
+
+const IdentityProfile: React.FC<{
+  identity: Identity,
+  protocols: DwnProtocolDefinition[],
+  permissions: DwnPermissionGrant[]
+}> = ({ identity, protocols, permissions }) => {
+
+  const grantees = useMemo(() => {
+    return [...(permissions.reduce((acc, permission) => {
+      if (!acc.has(permission.grantee)) {
+        acc.add(permission.grantee);
+      }
+
+      return acc;
+    }, new Set<string>()))];
+  }, [ permissions ]);
+
+
+  const heroStyle = useMemo(() => {
+    if (identity.profile.heroUrl) {
+      return {
+        backgroundPosition: 'bottom',
+        backgroundImage: `url(${identity.profile.heroUrl})`,
+        backgroundSize: 'cover',
+      }
+    } else {
+      return {
+        backgroundSize: 'cover',
+        background: generateGradient(identity.didUri.split(':')[2]),
+      }
+    }
+  }, [identity.profile.heroUrl, identity.didUri]);
+
+  return (
+    <main className="h-full">
+      <section className="relative block h-96">
+        <div
+          className="absolute top-0 w-full h-full bg-center bg-cover"
+          style={heroStyle}
+        >
+          <span id="blackOverlay" className="w-full h-full absolute opacity-50 bg-black"></span>
+        </div>
+      </section>
+      <section className="relative bg-blueGray-200 flex flex-col justify-start -mt-48">
+        <div className="flex flex-col break-words bg-white w-full mb-6 shadow-xl rounded-lg">
+          <div className="flex flex-wrap justify-center">
+            <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
+              <div className="relative -mt-16 w-32 h-32 flex items-center justify-center rounded-full shadow-xl bg-white overflow-hidden border-4 border-slate-300">
+                {identity.profile.avatarUrl ? (
+                  <img src={identity.profile.avatarUrl} />
+                ) : (
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0ZM3 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 019.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                </svg>
+              )}
+              </div>
+            </div>
+            <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
+              <div className="py-6 px-3 mt-32 sm:mt-0">
+                <button className="bg-slate-500 active:bg-slate-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
+                  Edit Profile
+                </button>
+              </div>
+            </div>
+            <div className="w-full lg:w-4/12 px-4 lg:order-1">
+              <div className="flex justify-center py-4 lg:pt-4 pt-8">
+                <div className="mr-4 p-3 text-center">
+                  <span className="text-xl font-bold block uppercase tracking-wide text-slate-600">{protocols.length}</span><span className="text-sm text-slate-400">Protocols</span>
+                </div>
+                {grantees.length > 0 && <div className="mr-4 p-3 text-center">
+                  <span className="text-xl font-bold block uppercase tracking-wide text-slate-600">{grantees.length}</span><span className="text-sm text-slate-400">Grantees</span>
+                </div>}
+              </div>
+            </div>
+          </div>
+          <div className="text-center mt-5">
+            <h3 className="text-4xl font-semibold leading-normal mb-2 text-slate-700 mb-2">
+              {identity.profile.social?.displayName}
+            </h3>
+            <div className="mb-2 text-slate-600 mt-2">
+              {identity.profile.social?.tagline}
+            </div>
+            <div className="mt-5 flex flex-row mx-auto justify-center">
+              <div className="mb-2 text-slate-400 sm:text-sm">
+                {identity.didUri}
+              </div>
+              <div className="text-slate-200">
+                <i className="fas fa-copy ml-2 text-lg text-slate-400"></i>
+              </div>
+              <div className="mb-2 text-slate-200">
+                <i className="fas fa-qrcode ml-2 text-lg text-slate-400"></i>
+              </div>
+            </div>
+          </div>
+          <div className="mt-1 py-10 border-t border-slate-300 text-center">
+            <div className="flex flex-wrap justify-center">
+              <div className="w-full lg:w-9/12 px-4">
+                <p className="mb-4 text-lg leading-relaxed text-slate-700">
+                  {identity.profile.social?.bio}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default IdentityProfile;
--- src/layoutes/TailwindDashboard.tsx
+++ src/layoutes/TailwindDashboard.tsx
@@ -6,42 +6,31 @@ import Header from '@/components/Layout/Header';
 import BottomNav from '@/components/Layout/BottomNav';
 import { useNavigate, Outlet } from "react-router-dom";
 import { useAgent } from '@/contexts/Context';
+import { MenuItemProps } from "@/components/Layout/Sidebar/SidebarItem";
 
 const TailwindLayout: React.FC = () => {
   const { lock } = useAgent();
   const navigate = useNavigate();
 
-  const menuItems = [{
+  const identityMenu: MenuItemProps = {
     onClick: () => navigate('/'),
     icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
     </svg>),
     children: (<span>Identities</span>),
     pattern: /(^\/$)|(^\/identity\/*.+)/
-  },{
+  };
+
+  const searchMenu: MenuItemProps = {
     onClick: () => navigate('/search'),
     icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
     </svg>),
     children: (<span>Find DIDs</span>),
     pattern: /^\/search?(\/*.)/
-  },{
-    children: <div className="h-1 w-full border-b border-gray-800 pt-3" />
-  },{
-    onClick: () => navigate('/identities/create'),
-    icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
-    </svg>),
-    children: (<span>Create Identity</span>),
-    pattern: /^\/identities\/create$/
-  },{
-    onClick: () => navigate('/identities/import'),
-    icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-    </svg>),
-    children: (<span>Import Identities</span>),
-    pattern: /^\/identities\/import$/
-  }, {
+  };
+
+  const appConnectMenu: MenuItemProps = {
     onClick: () => navigate('/app-connect'),
     icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
@@ -49,10 +38,27 @@ const TailwindLayout: React.FC = () => {
     </svg>),
     children: (<span>App Connect</span>),
     pattern: /^\/app-connect$/
-  }, {
-    children: <div className="h-1 w-full border-b border-gray-800" />,
-    bottom: true
-  },{
+  };
+
+  const createIdentityMenu: MenuItemProps = {
+    onClick: () => navigate('/identities/create'),
+    icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+    </svg>),
+    children: (<span>Create Identity</span>),
+    pattern: /^\/identities\/create$/
+  };
+
+  const importIdentityMenu: MenuItemProps = {
+    onClick: () => navigate('/identities/import'),
+    icon: (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+    </svg>),
+    children: (<span>Import Identities</span>),
+    pattern: /^\/identities\/import$/
+  };
+
+  const logoutMenu: MenuItemProps = {
     onClick: async () => {
       await lock();
       navigate('/');
@@ -61,8 +67,16 @@ const TailwindLayout: React.FC = () => {
       <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm-4 7a7 7 0 0 0-7 7h14a7 7 0 0 0-7-7Z" />
     </svg>),
     children: (<span>Log Out</span>),
-    pattern: /^\/logout$/
-  }];
+  };
+
+  const sidebarMenuItems = [identityMenu, searchMenu, {
+    children: <div className="h-1 w-full border-b border-gray-800 pt-3" />
+  }, appConnectMenu, createIdentityMenu, importIdentityMenu, {
+    children: <div className="h-1 w-full border-b border-gray-800" />,
+    bottom: true
+  }, logoutMenu];
+
+  const bottomNavMenuItems = [identityMenu, searchMenu, appConnectMenu, createIdentityMenu, importIdentityMenu];
 
   const logoIcon = (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-10">
     <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-2.25-1.313M21 7.5v2.25m0-2.25-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3 2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75 2.25-1.313M12 21.75V19.5m0 2.25-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25" />
@@ -75,17 +89,17 @@ const TailwindLayout: React.FC = () => {
       <Sidebar
         icon={logoIcon}
         header={header}
-        items={menuItems}
+        items={sidebarMenuItems}
       />
       <div className="flex h-full flex-1 flex-col">
-        <div className="flex h-full flex-col justify-between overflow-y-scroll">
-          <Header />
+        <div className="flex h-full flex-col justify-start overflow-y-scroll">
+          <Header icon={logoIcon}>{header}</Header>
           <Content>
             <Outlet />
           </Content>
           <Footer />
         </div>
-        <BottomNav />
+        <BottomNav menuItems={bottomNavMenuItems} />
       </div>
     </div>
   );
