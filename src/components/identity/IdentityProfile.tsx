acd31692b5d82f3be0f3628d92e5ad1376ebab7b
Liran Cohen <c.liran.c@gmail.com>
2024-10-08 17:10:27 -0400

    profile page

--- src/components/identity/IdentityProfile.tsx
+++ src/components/identity/IdentityProfile.tsx
@@ -30,13 +30,11 @@ export function generateGradient(byteString: string) {
 
 }
 
-
 const IdentityProfile: React.FC<{
   identity: Identity,
   protocols: DwnProtocolDefinition[],
   permissions: DwnPermissionGrant[]
 }> = ({ identity, protocols, permissions }) => {
-
   const navigate = useNavigate();
 
   const grantees = useMemo(() => {
@@ -49,6 +47,26 @@ const IdentityProfile: React.FC<{
     }, new Set<string>()))];
   }, [ permissions ]);
 
+  const bio = useMemo(() => {
+    return identity.profile.social?.bio
+  }, [ identity ]);
+
+  const displayName = useMemo(() => {
+    return identity.profile.social?.displayName;
+  }, [ identity ]);
+
+  const tagline = useMemo(() => {
+    return identity.profile.social?.tagline;
+  }, [ identity ]);
+
+  const apps = useMemo(() => {
+    const apps = identity.profile.social?.apps;
+    if (!apps || Object.keys(apps).length === 0) {
+      return undefined;
+    }
+
+    return apps;
+  } , [ identity ]);
 
   const heroStyle = useMemo(() => {
     if (identity.profile.heroUrl) {
@@ -75,61 +93,82 @@ const IdentityProfile: React.FC<{
           <span id="blackOverlay" className="w-full h-full absolute opacity-50 bg-black"></span>
         </div>
       </section>
-      <section className="relative bg-blueGray-200 -mt-48 sm:px-8 md:px-12 max-w-screen-lg mx-auto">
-        <div className="flex flex-col break-words bg-white w-full mb-6 shadow-xl rounded-lg">
-          <div className="flex flex-wrap justify-center">
-            <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-              <div className="relative -mt-16 w-32 h-32 flex items-center justify-center rounded-full shadow-xl bg-white overflow-hidden border-4 border-slate-300">
-                {identity.profile.avatarUrl ? (
-                  <img src={identity.profile.avatarUrl} />
-                ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0ZM3 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 019.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+      <section className="relative -mt-28 sm:px-8 md:px-12 max-w-screen-lg mx-auto">
+        <div className="flex flex-col break-words bg-white w-full mb-6 shadow-xl">
+          <div className="flex justify-between relative ml-5">
+            <div className="relative -mt-16 w-36 h-36 flex items-center rounded justify-center shadow-xl overflow-hidden border-8 border-gray-900 border-opacity-40">
+              {identity.profile.avatarUrl ? (
+                <img src={identity.profile.avatarUrl} />
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0ZM3 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 019.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
                 </svg>
               )}
-              </div>
             </div>
-            <div className="w-full lg:w-4/12 px-4 lg:order-3 md:text-right md:self-center">
-              <button onClick={() => navigate(`/identity/edit/${identity.didUri}`)} className="bg-slate-500 active:bg-slate-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
+            <div className="flex flex-col mt-5 mr-3">
+              <div
+                onClick={() => navigate(`/identity/edit/${identity.didUri}`)}
+                className="bg-gray-900 active:bg-gray-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+              >
                 Edit Profile
-              </button>
-            </div>
-            <div className="w-full lg:w-4/12 px-4 lg:order-1">
-              <div className="flex justify-center py-4 lg:pt-4 pt-8">
-                <div className="mr-4 p-3 text-center">
-                  <span className="text-xl font-bold block uppercase tracking-wide text-slate-600">{protocols.length}</span><span className="text-sm text-slate-400">Protocols</span>
-                </div>
-                {grantees.length > 0 && <div className="mr-4 p-3 text-center">
-                  <span className="text-xl font-bold block uppercase tracking-wide text-slate-600">{grantees.length}</span><span className="text-sm text-slate-400">Grantees</span>
-                </div>}
               </div>
             </div>
           </div>
-          <div className="text-center mt-5">
-            <h3 className="text-4xl font-semibold leading-normal mb-2 text-slate-700 mb-2">
-              {identity.profile.social?.displayName}
-            </h3>
-            <div className="mb-2 text-slate-600 mt-2">
-              {identity.profile.social?.tagline}
+          <div className="text-left mt-2 mx-8">
+            <div className="flex flex-row items-center mt-1">
+              <h3 className="text-3xl font-semibold leading-normal text-slate-700 mb-2">
+                {displayName}
+              </h3>
+              <div className="text-slate-400 ml-3 cursor-pointer hover:text-slate-600 transition-colors duration-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+                </svg>
+              </div>
+              <div className="text-slate-400 ml-1 cursor-pointer hover:text-slate-600 transition-colors duration-200">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 16.5h.75v.75h-.75v-.75Z" />
+                </svg>
+              </div>
             </div>
-            <div className="mt-5 flex flex-row mx-auto justify-center flex-wrap px-4">
-              <div className="mb-2 text-slate-400 sm:text-sm break-all">
-                {identity.didUri}
-              </div>
-              <div className="text-slate-200 ml-2">
-                <i className="fas fa-copy text-lg text-slate-400 cursor-pointer hover:text-slate-600 transition-colors duration-200"></i>
-              </div>
-              <div className="mb-2 text-slate-200 ml-2">
-                <i className="fas fa-qrcode text-lg text-slate-400 cursor-pointer hover:text-slate-600 transition-colors duration-200"></i>
-              </div>
+            <div className="mb-2 text-slate-600 px-1">
+              {tagline}
             </div>
           </div>
-          <div className="mt-1 py-10 border-t border-slate-300 text-center">
+          <div className="mt-5 flex bg-gray-300 flex-wrap justify-start border border-2 border-x-0 border-y-gray-500 h-12">
+            <div className="bg-gray-300 group flex flex-col justify-between">
+              <div
+                className="text-gray-900 px-6 pt-3 font-semibold"
+              >
+                Profile
+              </div>
+              <div className="w-full relative -bottom-[2px] h-[3px] bg-gray-900"></div>
+            </div>
+          </div>
+          <div className="mt-1 py-8 text-center">
             <div className="flex flex-wrap justify-center">
-              <div className="w-full lg:w-9/12 px-4">
-                <p className="mb-4 text-lg leading-relaxed text-slate-700">
-                  {identity.profile.social?.bio}
-                </p>
+              <div className="w-full lg:w-9/12 px-4 divide-y-2 divide-dotted divide-slate-300 mb-10">
+                <div className="text-xl text-left pl-4">
+                  About
+                </div>
+                {bio && <p className="mb-4 px-4 pt-4 text-md text-left leading-relaxed text-slate-600">
+                  {bio}
+                </p> || <p className="pt-4 mb-4 text-sm leading-relaxed text-slate-400 italic">Tell people about yourself</p>}
+              </div>
+              <div className="w-full lg:w-9/12 px-4 divide-y-2 divide-dotted divide-slate-300">
+                <div className="text-xl text-left pl-4">
+                  Social
+                </div>
+                {apps && <div className="mb-4 text-md leading-relaxed text-slate-700">
+                  {apps.facebook && <span className="mr-2">Facebook</span>}
+                  {apps.twitter && <span className="mr-2">Twitter</span>}
+                </div> || <p className="pt-4 mb-4 text-sm leading-relaxed text-slate-400 italic">Add social links</p>}
+              </div>
+              <div className="w-full lg:w-9/12 px-4 divide-y-2 divide-dotted divide-slate-300">
+                <div className="text-xl text-left pl-4">
+                  Career
+                </div>
+                <p className="pt-4 mb-4 text-sm leading-relaxed text-slate-400 italic">Where have you worked?</p>
               </div>
             </div>
           </div>
