cefd833d7bf3166c9532fce3d8f06db5a45a0a35
Liran Cohen <c.liran.c@gmail.com>
2024-10-10 11:21:42 -0400

    modal update

--- src/components/Layout/Dialog.tsx
+++ src/components/Layout/Dialog.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react'
 
 const DialogBox: React.FC<{ children: React.ReactNode, open: boolean, setOpen: (value: boolean) => void }> = ({ children, open, setOpen }) => {
 
@@ -10,10 +10,10 @@ const DialogBox: React.FC<{ children: React.ReactNode, open: boolean, setOpen: (
     />
 
     <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-      <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
         <DialogPanel
           transition
-          className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+          className="max-w-md transform overflow-hidden p-4 rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
         >
           {children}
         </DialogPanel>
